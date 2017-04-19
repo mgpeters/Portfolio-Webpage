e384b62ccb55af2055d63faf1970c4df52498674
Matt Peters <matthew.george.peters@gmail.com>
2017-04-19 18:15:08 -0400

    touch ups

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,6 @@ toggle between hiding and showing the dropdown content */
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
-
 function filterFunction() {
     var input, filter, ul, li, a, i;
     input = document.getElementById("myInput");
@@ -17,4 +16,17 @@ function filterFunction() {
             a[i].style.display = "none";
         }
     }
+}
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
 }
