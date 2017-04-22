3543cc33cee28a296733bfd38793c80de311d6de
Matt Peters <matthew.george.peters@gmail.com>
2017-04-22 18:08:23 -0400

    Added <div> per section

--- scripts/script.js
+++ scripts/script.js
@@ -3,20 +3,6 @@ toggle between hiding and showing the dropdown content */
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
-function filterFunction() {
-    var input, filter, ul, li, a, i;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    div = document.getElementById("myDropdown");
-    a = div.getElementsByTagName("a");
-    for (i = 0; i < a.length; i++) {
-        if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
-            a[i].style.display = "";
-        } else {
-            a[i].style.display = "none";
-        }
-    }
-}
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
@@ -32,7 +18,7 @@ window.onclick = function(event) {
 }
 // JQuery 
 $ (".about").click(function(){
-    $ (".about-content").show();
+        $ ("#about-content").fadeIn([75],[]);
 });
 
 $ (".alert-btn").click(function()   {
