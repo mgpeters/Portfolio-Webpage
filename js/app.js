c8e0472881e32f80d9e381e13073cc9f1d8d0e62
Matt Peters <matthew.george.peters@gmail.com>
2017-07-11 19:41:55 -0400

    playing with color scroll

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
 $(document).foundation().ready(function(){
     $(document).scroll(function() {
         var alpha = Math.min(0.10 + 0.4 / $(this).scrollTop() * 1000, 0.9);
-        var channel = Math.round(alpha * 255);
+        var channel = Math.round(alpha * 250);
         $("body").css('background-color', 'rgb(' + channel + ',' + channel + ',' + channel + ')');
+    });
+    $(document).scroll(function() {
+        var alpha = Math.min(0.10 + 0.4 * $(this).scrollTop() / 1000, 0.9);
+        var channel = Math.round(alpha * 250);
+        $(".text").css('color', 'rgb(' + channel + ',' + channel + ',' + channel + ')');
     });
 });
