db7be648728513e8369919554120bba333946b26
Matt Peters <matthew.george.peters@gmail.com>
2017-07-11 19:25:23 -0400

    color change scroll jQuery

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
-$(document).foundation()
+$(document).foundation().ready(function(){
+    $(document).scroll(function() {
+        var alpha = Math.min(0.10 + 0.4 / $(this).scrollTop() * 1000, 0.9);
+        var channel = Math.round(alpha * 255);
+        $("body").css('background-color', 'rgb(' + channel + ',' + channel + ',' + channel + ')');
+    });
+});
